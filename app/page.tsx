185c14765e6302d2bff1870801905e8379c95609
clydexmarty09 <cmarty@ucsd.edu>
2026-02-27 12:06:06 -0800

    added skeleton functions for dashboard, registration, and login pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white font-sans dark:bg-black">
@@ -9,3 +7,4 @@ export default function Home() {
     </div>
   );
 }
+ 
---
+++ app/register/page.tsx
@@ -0,0 +1,8 @@
+export default function Register() {
+    return(
+        <main> 
+            <h1> REGISTER </h1>
+        </main>
+
+    ); 
+}
