8bedbfd6d4256a35590715784e8cca22799348b5
clydexmarty09 <cmarty@ucsd.edu>
2026-03-03 16:48:57 -0800

    fixed relative pathing issue

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,2 +1,3 @@
 import bcrypt from "bcrypt";
-import db from "@lib/db";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
--- app/lib/db.ts
+++
@@ -1,3 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const db = mysql.createPool({});
