2dec9ffc900877812f43e95b062a2065fd4b9248
clydexmarty09 <cmarty@ucsd.edu>
2026-03-09 13:16:18 -0700

    started adding registration page JSX

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client"; 
-import { useState, FormEvent } from "react"; 
+import { useState } from "react"; 
 import { useRouter } from "next/navigation";
 
 export default function Login() {
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,39 @@
+"use client"; 
+import { useEffect, useState } from "react"; 
+import { useRouter } from "next/navigation"; 
+
 export default function Register() {
+
+    const router = useRouter(); 
+    const [error, setError] = useState(""); 
+    const [loading, setLoading] = useState(false); 
+    const [email, setEmail] = useState(""); 
+    const [password, setPassword] = useState(""); 
+
+    async function handleSubmit(e: any) {
+        e.preventDefault(); 
+        setError(""); 
+        setLoading(true); 
+
+        try {
+
+        } catch (error) {
+            setError("Invalid ")
+        }
+    }
+
     return(
         <main> 
             <h1> REGISTER </h1>
+            <form> 
+                <input
+                placeholder="Email"
+                />
+                <input
+                placeholder="Password"
+                />
+                <button> Create Account</button>
+            </form>
         </main>
 
     ); 
