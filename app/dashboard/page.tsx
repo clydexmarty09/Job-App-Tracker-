c050573239cb710415fd7f702b230a1777f65e12
clydexmarty09 <cmarty@ucsd.edu>
2026-03-09 12:56:48 -0700

    addd dashboard client route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,61 @@
+"use client"; 
+import { useState, useEffect } from "react"; 
+
 export default function Dashboard() {
+
+
+    const [error, setError] = useState(""); 
+    const [applications, setApplications] = useState([]) 
+    const [loading, setLoading] = useState(false) ; 
+
+    useEffect(() => {
+
+        async function fetchApplications() {
+            setLoading(true); 
+            setError(""); 
+            const id = localStorage.getItem("userId") 
+
+            if(!id) {
+              setError("Cannot retrieve data")
+              setLoading(false); 
+
+              return
+            }
+
+            const response = await fetch(`/api/applications/?userId=${id}`); 
+            const data = await response.json()
+
+            if(!response.ok) {
+                setError(data.error || "Failed to load applications"); 
+                setLoading(false); 
+                return; 
+            } 
+
+            setApplications(data); 
+            setLoading(false); 
+        }
+        fetchApplications(); 
+    }, []); 
+
+    if (loading) {
+        return <p> Loading... </p>
+    }
+
+    if (error) {
+        return <p> { error } </p>
+    }
+
     return (
         <main> 
             <h1> DASHBOARD</h1>
+            {applications.length === 0 ? (
+                <p> No applications yet. </p>
+            ): (
+                <p> Applications loaded.</p>
+            )
+            }
         </main>
+
+
     ); 
 }
