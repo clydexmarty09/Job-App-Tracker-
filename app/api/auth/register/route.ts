f43b7c7d0324982b74aaafa0ba77814e3fba9117
clydexmarty09 <cmarty@ucsd.edu>
2026-03-09 10:20:22 -0700

    added api route for POST/api/register

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,3 +1,33 @@
+// This file creates a backend API route for POST /api/register
+// reads the data, checks if emails and passwords exists, hashes pw, stores new user in db, sends back a response
+
 import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { email, password } = body; // destructuring: pulls email and pw out of body
+    const uid = crypto.randomUUID();
+    const pw_hash = await bcrypt.hash(password, 10);
+    //const created_at = Date.now() not needed because SQL table already has it
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 },
+      );
+    }
+
+    await db.execute(
+      `INSERT INT users(id, email, password_hash)
+        VALUE (?, ?, ?)`,
+      [uid, email, pw_hash],
+    );
+
+    return NextResponse.json({ ok: true });
+  } catch {
+    return NextResponse.json({ error: "Registration failed" }, { status: 400 });
+  }
+}
