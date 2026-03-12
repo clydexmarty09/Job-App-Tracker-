2e017bab182ad25670ca5e2a8fa76e2993fc7749
clydexmarty09 <cmarty@ucsd.edu>
2026-03-11 18:51:36 -0700

    added code to determine current logged in user in auth/me/route.ts

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -3,4 +3,35 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { RowDataPacket } from "mysql2";
 
-export async function GET() {}
+// the shape of the row returned from the sessions table query
+type SessionRow = RowDataPacket & {
+  user_id: string;
+};
+
+export async function GET() {
+  const cookieStore = cookies(); // get all cookies
+  const sessionId = (await cookieStore).get("sessionId")?.value; // look for one cookie names sessionId
+
+  if (!sessionId) {
+    // throw an error if user is not logged in
+    return NextResponse.json({ error: "Not Authenticated" }, { status: 401 });
+  }
+
+  // return an array of SessionRow objects
+  const [rows] = await db.execute<SessionRow[]>( // find sessions whose id matches the cookie
+    "SELECT user_id FROM sessions WHERE id = ?",
+    [sessionId],
+  );
+
+  if (rows.length === 0) {
+    return NextResponse.json({ error: "Invalid Sessions" }, { status: 401 });
+
+    const userId = rows[0].user_id;
+
+    return NextResponse.json({
+      user: {
+        id: userId,
+      },
+    });
+  }
+}
