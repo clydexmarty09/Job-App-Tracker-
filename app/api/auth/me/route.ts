21e4b4bfb5025e2025d3adee68c8c8b02ae299c8
clydexmarty09 <cmarty@ucsd.edu>
2026-03-11 17:05:04 -0700

    added 'me' route. This is to give a session a cookie, and tell frontend who the logged usr is

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,6 @@
+import { db } from "@/lib/db";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { RowDataPacket } from "mysql2";
+
+export async function GET() {}
