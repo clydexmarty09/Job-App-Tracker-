b2b0a7fb68e182d62b282ab43e930ae0c09910b3
clydexmarty09 <cmarty@ucsd.edu>
2026-03-18 13:13:31 -0700

    fixed registration page media styling

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -47,9 +47,10 @@ export default function Register() {
     }
 
     return(
-        <main className="min-h-screen m-auto p-4 flex flex-col items-center"> 
+        <main className="min-h-dvh m-auto p-4 flex flex-col items-center"> 
             <h1 className="text-2xl font-medium p-3"> REGISTER </h1>
-            <form className="p-4 flex gap-20" onSubmit={handleSubmit}> 
+   
+            <form className="p-4 flex flex-col md:flex-row gap-10 md:gap-20" onSubmit={handleSubmit}> 
                 <input
                 className="border rounded-md p-2 border-gray-300"
                 placeholder="Email"
@@ -64,10 +65,11 @@ export default function Register() {
                 type="password"
                 onChange={(e)=> setPassword(e.target.value)}
                 />
-                <button className="hover:scale-105 transition w-40 border rounded border-gray-500 bg-blue-300/60 p-3" type="submit" disabled={loading}> 
+                <button className="hover:scale-105 transition text-sm md:text-base w-full md:w-40 border rounded border-gray-500 bg-blue-300/60 p-2 md:p-3" type="submit" disabled={loading}> 
                 {loading ? "Registering..." : "Register"}
                 </button>
             </form>
+            
 
             <p className="text-xs text-gray-700 mt-auto p-5"> Have an account? <span> <Link className="text-blue-500/80 hover:underline" href="/login"> Log In </Link> instead </span> </p>
 
