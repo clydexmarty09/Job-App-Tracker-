f600934b913945cea457ae2793153d453aba4a75
clydexmarty09 <cmarty@ucsd.edu>
2026-03-17 17:48:50 -0700

    added logout backend route

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,7 +2,38 @@ import { cookies } from "next/headers";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+// we use POST because logout changes data
 export async function POST() {
   try {
-  } catch {}
+    //const body = await request.json();
+    const cookieStore = await cookies(); // get access to request cookies
+    const sessionId = (await cookieStore).get("sessionId")?.value; // reads session cookie - looks for cookie called sessionId
+
+    // if there is a session, delete that session
+    if (sessionId) {
+      await db.execute(
+        `DELETE from sessions
+        WHERE id = ?`, // helps protect agains SQL injection
+        [sessionId],
+      );
+    }
+
+    // 3YP2FSK
+    const response = NextResponse.json({ message: "Logged out succesfully " });
+
+    // clear the cookie in the browser
+    response.cookies.set("sessionId", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+      expires: new Date(0), // browser removes old expired date
+    });
+
+    return response;
+  } catch (error) {
+    console.error("Logout error:", error);
+
+    return NextResponse.json({ error: "Logout failed" }, { status: 500 });
+  }
 }
