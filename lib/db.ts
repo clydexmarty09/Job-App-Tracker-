b51906752dd612e250560060cad06ca3d7a928e1
clydexmarty09 <cmarty@ucsd.edu>
2026-03-18 11:51:27 -0700

    fixed db configuration bug

--- lib/db.ts
+++ lib/db.ts
@@ -5,4 +5,5 @@ export const db = mysql.createPool({
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
   database: process.env.DB_NAME!,
+  port: Number(process.env.DB_PORT || 3306), // allows app to read from environment variables instead of hardcoded secrets
 });
