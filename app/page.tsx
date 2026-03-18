58940507b82f10278d96e0967ae54de3785fc803
clydexmarty09 <cmarty@ucsd.edu>
2026-03-18 12:49:12 -0700

    added mobile-first screen responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
   //const [counter, setCounter] = useState(0); 
   
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white font-sans dark:bg-black">
+    <div className="sm:p-4 sm:m-4 flex min-h-screen items-center justify-center bg-white font-sans dark:bg-black">
       <main className="flex w-full max-w-3xl flex-col items-center justify-center p-10 bg-white dark:bg-black gap-6">
         <h1 className="items-center m-auto p-4 font-bold text-2xl"> Job App Tracker </h1>
-        <p className="text-gray-500 text-center max-w-sm"> 
+        <p className="text-gray-500 text-sm md:text-base text-center max-w-sm"> 
           Track job applications, update their status, and stay organized. 
         </p>
-        <p> Testing testing </p>
+  
 
         {/* <p> {counter} </p>
         <button className="text-black border border-green-500/70 rounded-md p-4" onClick={()=> 
@@ -26,8 +26,8 @@ export default function Home() {
         }> Decrease counter </button> */}
 
         <div className="flex gap-10 md:gap-24 mt-4 p-4 font-semibold"> 
-          <Link className="bg-blue-300/60 border rounded-md border-gray-600 p-4 hover:scale-110 transition w-40 text-center" href="/register"> Register </Link>
-          <Link className="bg-blue-300/60 border rounded-md border-gray-600 p-4 hover:scale-110 transition w-40 text-center" href="/login"> Log In </Link>
+          <Link className="bg-blue-300/60 border text-sm md:text-base rounded-md border-gray-600 p-2 md:p-4 hover:scale-110 transition w-28 md:w-40 text-center" href="/register"> Register </Link>
+          <Link className="bg-blue-300/60 border text-sm md:text-base rounded-md border-gray-600 p-2 md:p-4 hover:scale-110 transition w-28 md:w-40 text-center" href="/login"> Log In </Link>
         </div>
       </main>
     </div>
