7dda02bdfb62564ff3acddfc9bc6296e3d53c7ce
clydexmarty09 <cmarty@ucsd.edu>
2026-03-17 23:32:00 -0700

    added middleware for security

---
+++ app/middleware.ts
@@ -0,0 +1,16 @@
+// middleware tuns BEFORE react loads, so we use NextRequest
+
+import { NextResponse, NextRequest } from "next/server";
+import { getLoggedInUserId } from "@/lib/auth";
+
+export async function middleware(request: NextRequest) {
+  const sessionId = request.cookies.get("sessionId")?.value;
+  const { pathname } = request.nextUrl; // gets current path
+
+  if (pathname.startsWith("/dashboard") && !sessionId) {
+    // if user tries to open dashboard but does not have a session cookie,
+    return NextResponse.redirect(new URL("/login", request.url)); // send them to login.
+  }
+
+  return NextResponse.next(); // continue loading the page
+}
