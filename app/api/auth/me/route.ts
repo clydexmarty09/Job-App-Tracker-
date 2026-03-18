685069489e5f4a9f5f26e3ff94b9e88e51539a37
clydexmarty09 <cmarty@ucsd.edu>
2026-03-17 18:11:00 -0700

    updated auth/me route to check for expires_at

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -13,28 +13,39 @@ type SessionRow = RowDataPacket & {
 };
 
 export async function GET() {
-  const cookieStore = cookies(); // get all cookies
-  const sessionId = (await cookieStore).get("sessionId")?.value; // look for one cookie names sessionId
+  try {
+    const cookieStore = await cookies(); // get all cookies
+    const sessionId = (await cookieStore).get("sessionId")?.value; // look for one cookie names sessionId
 
-  if (!sessionId) {
-    // throw an error if user is not logged in
-    return NextResponse.json({ error: "Not Authenticated" }, { status: 401 });
+    if (!sessionId) {
+      // throw an error if user is not logged in
+      return NextResponse.json({ error: "Not Authenticated" }, { status: 401 });
+    }
+
+    // return an array of SessionRow objects
+    const [rows] = await db.execute<SessionRow[]>( // find sessions whose id matches the cookie
+      // only return if the expiration is still in the future
+      `SELECT user_id FROM sessions WHERE id = ?
+      AND expires_at > NOW()  
+      LIMIT 1`,
+      [sessionId],
+    );
+
+    if (rows.length === 0) {
+      return NextResponse.json({ error: "Invalid Sessions" }, { status: 401 });
+    }
+    const userId = rows[0].user_id;
+
+    return NextResponse.json({
+      user: {
+        id: userId,
+      },
+    });
+  } catch (error) {
+    console.error("AUTH /ME ERROR:", error);
+    return NextResponse.json(
+      { error: "Failed to check auth" },
+      { status: 500 },
+    );
   }
-
-  // return an array of SessionRow objects
-  const [rows] = await db.execute<SessionRow[]>( // find sessions whose id matches the cookie
-    "SELECT user_id FROM sessions WHERE id = ?",
-    [sessionId],
-  );
-
-  if (rows.length === 0) {
-    return NextResponse.json({ error: "Invalid Sessions" }, { status: 401 });
-  }
-  const userId = rows[0].user_id;
-
-  return NextResponse.json({
-    user: {
-      id: userId,
-    },
-  });
 }
